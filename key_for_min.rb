a94159aff2042ccae7ebaa64b7855f60b9b97387
Samantha Wabi <swfullpotential@gmail.com>
2020-05-29 19:47:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  key_for_min_value.collect do |name, item|
-    name 
+  key_for_min_value.collect do |name, num|
+    name
 end
 
 
